f7201f0c8eda6dcdd9f701617d4adf645647f362
smirnov.boris <b.smirnov@rusgates.ru>
2021-05-14 21:39:30 +0300

    staging stage added

--- final_task/provide_cloud_machines.tf
+++ final_task/provide_cloud_machines.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "newCloudMachine" {
+resource "aws_instance" "buildMachine" {
   ami           = "ami-08962a4068733a2b6"
   instance_type = "t2.micro"
 
@@ -23,11 +23,28 @@ resource "aws_instance" "newCloudMachine" {
 #  user_data = "${file("post_provide.sh")}"
 
   tags = {
-    Name = "newCloudMachine"
+    Name = "buildMachine"
   }
 
   provisioner "local-exec" {
-    command = "rm -f /home/final_task/cloud_machine_ip.info && echo ${self.public_ip} >> /home/final_task/cloud_machine_ip.info && echo newCloudMachine ip is ${self.public_ip}"
+    command = "rm -f /home/final_task/build_machine_ip.info && echo ${self.public_ip} >> /home/final_task/build_machine_ip.info && echo buildMachine ip is ${self.public_ip}"
+  }
+
+}
+
+resource "aws_instance" "stageMachine" {
+  ami           = "ami-08962a4068733a2b6"
+  instance_type = "t2.micro"
+
+  key_name = "aws3"
+  security_groups = ["groupAnsible11"]
+
+  tags = {
+    Name = "stageMachine"
+  }
+
+  provisioner "local-exec" {
+    command = "rm -f /home/final_task/stage_machine_ip.info && echo ${self.public_ip} >> /home/final_task/stage_machine_ip.info && echo stageMachine ip is ${self.public_ip}"
   }
 
 }
